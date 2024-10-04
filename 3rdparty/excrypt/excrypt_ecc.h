25e4f2477d491b75372cd836eca9e554b700629b
emoose <abc@cock.li>
2024-10-04 15:29:23 +0100

    3rdparty: remove excrypt headers

--- 3rdparty/excrypt/excrypt_ecc.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-uint32_t ExCryptEccGetCurveParameters(uint32_t curve_num, uint32_t** curve_param);
--- 3rdparty/excrypt/excrypt_mem.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-int32_t ExCryptMemDiff(uint8_t* buf1, uint8_t* buf2, uint32_t size);
-
-// (not from XeCrypt)
-// Reverses the bytes in the data buffer
-void ExCryptMemReverseBytes(uint8_t* data, uint32_t length);
